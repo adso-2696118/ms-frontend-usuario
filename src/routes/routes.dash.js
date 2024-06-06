22de39f01bde5279be86d5b1f501b25e73dbb762
elingenieroprofe <SENA@ITAAPRRCDMCD16.FormacionSena.RED>
2024-06-06 11:24:02 -0500

    Se agrego el dash y lista la presentacion

--- src/routes/routes.dash.js
+++ src/routes/routes.dash.js
@@ -1,10 +1,10 @@
+import { render } from "ejs";
 import { Router } from "express";
+import { presentacion } from "../controllers/controllers.dash";
 
 const rutaDash = Router();
 
-rutaDash.get("/", (req, res)=>{
-    res.send("<h1> Este es mi dashboard </h1> ");
-})
+rutaDash.get("/", presentacion);
 rutaDash.get("/user", (req, res)=>{
     res.send("<h1> Usuarios </h1> ");
 })
