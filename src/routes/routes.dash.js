4ddc710e61a82be7b4b3128ff124a89b34c86403
elingenieroprofe <elingenieroprofesor@gmail.com>
2024-06-13 08:02:07 -0500

    Pregunta nataly

--- src/routes/routes.dash.js
+++ src/routes/routes.dash.js
@@ -5,8 +5,5 @@ import { presentacion } from "../controllers/controllers.dash";
 const rutaDash = Router();
 
 rutaDash.get("/", presentacion);
-rutaDash.get("/user", (req, res)=>{
-    res.send("<h1> Usuarios </h1> ");
-})
 
 export default rutaDash;
